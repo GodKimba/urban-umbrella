5972c41b15df610c199b7f01544c7df0c5428904
GodKimba <rafaelfadel2@gmail.com>
2022-04-25 19:25:48 -0300

    finished the subject or sender parts of the program

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ username = config("USERNAME")
 password = config("PASSWORD")
 
 mail_server = "imap.gmail.com"
-sender = input("Enter the sender mail address: ").lower().strip()
 
 # Create an IMAP4 class with SSL
 imap = imaplib.IMAP4_SSL(mail_server)
@@ -18,33 +17,6 @@ imap.login(username, password)
 
 # Selecting mailbox to work with
 imap.select("inbox")
-
-# Searching for specific mails by sender
-status, messages = imap.search(None, f"FROM {sender}")
-
-# Convert messages to a list of email IDs
-messages = messages[0].split(b" ")
-
-# Loop to iterate over targeted mails and mark them as deleted
-for mail in messages:
-    _, msg = imap.fetch(mail, "(RFC822)")
-    # This second loop is only for printing the SUBJECT of targeted mails
-    for response in msg:
-        if isinstance(response, tuple):
-            msg = email.message_from_bytes(response[1])
-            # Decoding the mail subject
-            subject = decode_header(msg["Subject"])[0][0]
-            if isinstance(subject, bytes):
-                # If it is a bytes type, decode to str
-                subject = subject.decode()
-            print("Deleting", subject)
-    # Marking the mail as deleted
-    imap.store(mail, "+FLAGS", "\\Deleted")
-
-    # Permanently remove mails that are marked as deleted
-imap.expunge()
-imap.close()
-imap.logout()
 
 
 user_response = (
@@ -56,6 +28,7 @@ user_response = (
 if user_response == "sender":
     sender = input("Enter the sender mail address: ").lower().strip()
     status, messages = imap.search(None, f"FROM {sender}")
+    messages = messages[0].split(b" ")
     # Loop to iterate over targeted mails and mark them as deleted
     for mail in messages:
         _, msg = imap.fetch(mail, "(RFC822)")
@@ -71,10 +44,12 @@ if user_response == "sender":
                 print("Deleting", subject)
         # Marking the mail as deleted
         imap.store(mail, "+FLAGS", "\\Deleted")
+
 
 elif user_response == "subject":
-    subject = input("Enter the sender mail address: ").lower().strip()
+    subject = input("Enter the subject key-words: ").lower().strip()
     status, messages = imap.search(None, f"SUBJECT {subject}")
+    messages = messages[0].split(b" ")
     # Loop to iterate over targeted mails and mark them as deleted
     for mail in messages:
         _, msg = imap.fetch(mail, "(RFC822)")
@@ -91,4 +66,11 @@ elif user_response == "subject":
         # Marking the mail as deleted
         imap.store(mail, "+FLAGS", "\\Deleted")
 
-# testing
+# Else statement to prevent error in caso neither of the other two statemets are true (should i use try and except?)
+else: 
+    imap.close()
+
+
+imap.expunge()
+imap.close()
+imap.logout()
